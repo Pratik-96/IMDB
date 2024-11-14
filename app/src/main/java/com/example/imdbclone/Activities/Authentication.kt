6c0200b151ddac27579b9a22e87c8e17a0a87785
pratik <pratiknandvikar1404@gmail.com>
2024-11-14 19:36:41 +0530

    Added new Authentication Screen to authenticate user

---
+++ app/src/main/java/com/example/imdbclone/Activities/Authentication.kt
@@ -0,0 +1,41 @@
+package com.example.imdbclone.Activities
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.provider.FontsContractCompat.Columns
+import com.example.imdbclone.Screen.LargeText
+import com.example.imdbclone.ui.theme.IMDBCloneTheme
+
+class Authentication:ComponentActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            IMDBCloneTheme {
+
+            }
+        }
+    }
+}
+
+@Composable
+fun SignUp() {
+    Column(modifier = Modifier.fillMaxSize().background(color = Color.Gray)){
+        LargeText("SignUp")
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun AuthPrev() {
+    IMDBCloneTheme {
+        SignUp()
+    }
+}
