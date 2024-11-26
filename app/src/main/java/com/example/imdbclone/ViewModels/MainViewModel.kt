a21a874d36e1403c0fa027aa5114f6affc365a6b
Pratik <pratiknandvikar1404@gmail.com>
2024-11-26 19:58:40 +0530

    Updated the backend for VerticalImageSlider on HomePage.

--- app/src/main/java/com/example/imdbclone/ViewModels/MainViewModel.kt
+++ app/src/main/java/com/example/imdbclone/ViewModels/MainViewModel.kt
@@ -295,7 +295,7 @@ class MainViewModel : ViewModel() {
 
                         val genres =
                             snapshot.getValue(object : GenericTypeIndicator<List<String>>() {})
-                        selectedGenres = genres?.shuffled()?.take(2)?.joinToString(",").toString()
+                        selectedGenres = genres?.shuffled()?.take(3)?.joinToString(",").toString()
                         fetched = true
                         _genreState.value = _genreState.value.copy(
                             genres = selectedGenres,
